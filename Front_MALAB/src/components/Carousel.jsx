4163ef2ee7c15e132dcc379ea29c0bcd281845c8
Mario Aguilar <marioaguilar740@gmail.com>
2023-04-25 18:38:51 +0200

    Primera prueba para utilizar un carousel como componente, de momento no se ha logrado hacer responsive

---
+++ Front_MALAB/src/components/Carousel.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import './Carousel.css'
+
+export const Caroussel = () => {
+    return (
+        <>
+        <div className="carousel-wrapper">
+            <Carousel interval={5000} >
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://via.placeholder.com/400x200.png?text=First+Slide"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://via.placeholder.com/400x200.png?text=Second+Slide"
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://via.placeholder.com/400x200.png?text=Third+Slide"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+        </>
+    )
+}
--- Front_MALAB/src/layouts/home/Home.jsx
+++ Front_MALAB/src/layouts/home/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { Caroussel } from '../../components/Carousel'
 import { InputText } from '../../components/InputText'
 import { NavBar } from '../../components/NavBar'
 
 export const Home = () => {
-  return (
-    <>
-    <NavBar/>
-    <div>Home
-    <InputText/>
-    </div>
-    </>
-  )
+    return (
+        <>
+        <NavBar/>
+        <Caroussel/>
+        <div>Home
+        <InputText/>
+        </div>
+        </>
+    )
 }
