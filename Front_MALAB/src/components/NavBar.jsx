11fa0905bca7bcd09f3cc2d464182e9a2f07153b
Mario Aguilar <marioaguilar740@gmail.com>
2023-04-26 19:35:19 +0200

    Se han hecho modificaciones en la NavBar, aun falta agregar componentes para la ternaria

--- Front_MALAB/src/components/NavBar.jsx
+++ Front_MALAB/src/components/NavBar.jsx
@@ -3,29 +3,37 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { userData, userout } from '../layouts/userDetail/userSlice';
 
 export const NavBar = () => {
+
+    const credentialsRdx = useSelector(userData);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const logout = () => {
+        dispatch(userout({ credentials: {}, token: ''}));
+        return navigate("/");
+    }
     return (
         <>
         <Navbar bg="light" expand="lg">
         <Container>
-            <Navbar.Brand href="#home">MALAB</Navbar.Brand>
+            <Navbar.Brand as={Link} to={'/'}>MALAB</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                    Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                </NavDropdown.Item>
+                <Nav.Link href="#home">Coworking</Nav.Link>
+                <Nav.Link href="#link">Mesas</Nav.Link>
+                <Nav.Link href="#link">Tarifas</Nav.Link>
+                <Nav.Link href="#link">Contacto</Nav.Link>
+                <NavDropdown title="Únete a la Familia" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Soy miembro</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Quiero unirme</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Quiero visitar MALAB</NavDropdown.Item>
                 </NavDropdown>
+                <Nav.Link href="#link"></Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Container>
