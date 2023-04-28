32c2d9a37771120cab9ecb67d5f90d35e6de67a4
Mario Aguilar <marioaguilar740@gmail.com>
2023-04-28 11:26:04 +0200

    Correccion de la varible datosBackend L.142

--- Front_MALAB/src/layouts/login/Login.jsx
+++ Front_MALAB/src/layouts/login/Login.jsx
@@ -1,116 +1,160 @@
 import React, { useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { decodeToken } from 'react-jwt';
-import { useDispatch, useSelector } from 'react-redux';
-import { userData, login } from '../userDetail/userSlice';
+import { useDispatch } from 'react-redux';
+import {  login } from '../userDetail/userSlice';
 import { logMe } from '../../services/apiCalls';
 import { InputText } from '../../components/InputText';
 import { Container, Form, Button } from 'react-bootstrap';
-import { validate } from '../../helpers/useFul';
+// import { validate } from '../../helpers/useFul';
 import './Login.css'
+import { NavBar } from '../../components/NavBar';
 
 
 export const Login = () => {
 
-    const dispatch = useDispatch();
-    const credentialsRdx = useSelector(userData);
+//     const dispatch = useDispatch();
+//     const credentialsRdx = useSelector(userData);
 
-    const navigate = useNavigate();
-    const [welcome, setwelcome] = useState("");
+//     const navigate = useNavigate();
+//     const [welcome, setwelcome] = useState("");
 
-    const [credenciales, setcredenciales] = useState({
-        email: "",
-        password: ""
-    })
+//     const [credenciales, setcredenciales] = useState({
+//         email: "",
+//         password: ""
+//     })
+//     console.log(credenciales)
 
-    const inputHandler = (e) => {
-        setcredenciales((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-        }));
-    };
+//     const inputHandler = (e) => {
+//         setcredenciales((prevState) => ({
+//             ...prevState,
+//             [e.target.name]: e.target.value,
+//         }));
+//     };
 
-    const [credencialesError, setCredencialesError] = useState({
-        emailError: "",
-        passwordError: ""
-    });
+//     const [credencialesError, setCredencialesError] = useState({
+//         emailError: "",
+//         passwordError: ""
+//     });
 
-    const [valiCredenciales, setValiCredenciales] = useState({
-        emailVali: false,
-        passwordVali: false,
-    })
+//     const [valiCredenciales, setValiCredenciales] = useState({
+//         emailVali: false,
+//         passwordVali: false,
+//     })
 
-    const [loginAct, setLoginAct] = useState(false);
+//     console.log(valiCredenciales);
 
-    useEffect(() => {
-        for(let error in credencialesError){
-            if(credencialesError[error] !== ""){
-                setLoginAct(false);
-                return;
-            }
-        }
+//     const [loginAct, setLoginAct] = useState(false);
 
-        for(let vacio in credenciales){
-            if(credenciales[vacio] === ""){
-                setLoginAct(false);
-                return;
-            }
-        }
+//     useEffect(() => {
+//         for(let error in credencialesError){
+//             if(credencialesError[error] !== ""){
+//                 setLoginAct(false);
+//                 return;
+//             }
+//         }
 
-        for(let validation in valiCredenciales){
-            if(valiCredenciales[validation] === false){
-                setLoginAct(false);
-                return;
-            }
-        }
+//         for(let vacio in credenciales){
+//             if(credenciales[vacio] === ""){
+//                 setLoginAct(false);
+//                 return;
+//             }
+//         }
 
-        console.log(credenciales)
+//         for(let validation in valiCredenciales){
+//             if(valiCredenciales[validation] === false){
+//                 setLoginAct(false);
+//                 return;
+//             }
+//             console.log(validation);
+//         }
 
-        setLoginAct(true);
-    });
+//         setLoginAct(true);
+//     });
 
-    const checkError = (e) => {
-        let error = "";
+//     const checkError = (e) => {
+//         let error = "";
 
-        let checked = validate(
-            e.target.name,
-            e.target.value,
-            e.target.required
-        );
+//         let checked = validate(
+//             e.target.name,
+//             e.target.value,
+//             e.target.required
+//         );
 
-        error = checked.message;
+//         error = checked.message;
 
-        setValiCredenciales((prevState) => ({
-            ...prevState,
-            [e.target.name + "Vali"]: checked.validated,
-        }));
+//         setValiCredenciales((prevState) => ({
+//             ...prevState,
+//             [e.target.name + "Vali"]: checked.validated,
+//         }));
 
-        setCredencialesError((prevState) => ({
-            ...prevState,
-            [e.target.name + "Error"]: error,
-        }));
-    };
+//         setCredencialesError((prevState) => ({
+//             ...prevState,
+//             [e.target.name + "Error"]: error,
+//         }));
+//     };
+
+// const logeame = () => {
+
+//     logMe(credenciales)
+//     .then((respuesta) => {
+//         const { data } = respuesta
+//         const decodedToken = decodeToken(data.token)
+//         let datosBackend = {
+//             id_usuario: decodedToken.id,
+//             id_rol: decodedToken.id_rol,
+//             token: data.token
+//         };
+//         dispatch(login({ credentials: datosBackend}));
+//         setwelcome(`Bienvenidx de nuevo ${datosBackend.usuario}`);
+//         setTimeout(() => {
+//             navigate("/");
+//         }, 500);
+// console.log(datosBackend);
+
+//     })
+//     .catch ((error) => console.log(error));
+// };
+
+const navigate = useNavigate();
+const dispatch = useDispatch();
+const [welcome, setWelcome] = useState("");
+
+const [credenciales, setCredenciales] = useState({
+email: '',
+password: ''
+})
+
+const inputHandler = (e) => {
+    setCredenciales((prevState) => ({
+        ...prevState,
+        [e.target.name]: e.target.value,
+    }));
+}
+
+const checkError = (e) => {
+}
 
 const logeame = () => {
-
-    logMe(credenciales)
-    .then((respuesta) => {
-        const { data } = respuesta
-        const decodedToken = decodeToken(data.token)
-        let datosBackend = {
-            userId: decodedToken.userId,
-            rolId: decodedToken.rolId,
-            token: data.token
-        };
-        dispatch(login({ credentials: datosBackend}));
-        setwelcome(`Bienvenidx de nuevo ${datosBackend.usuario}`);
-        setTimeout(() => {
-            navigate("/");
-        }, 500);
+logMe(credenciales)
+    .then( respuesta => {
+        let decodificado = decodeToken(respuesta.data.token)
+            let datosBackend = {
+                token: respuesta.data.token,
+                id: decodificado.usuarioId,
+                id_rol: decodificado.rolId
+            };   
+            console.log(respuesta)    
+            dispatch(login({credentials: datosBackend}));
+            setWelcome(`Bienvenid@ de nuevo ${datosBackend.usuario}`);
+    setTimeout(() => {
+        navigate("/");
+        }, 3000);
+        console.log(respuesta);
+        console.log(datosBackend);
     })
-    .catch ((error) => console.log(error));
+    .catch(error => console.log(error))
 };
-
 
     return (
         <>
@@ -119,9 +163,7 @@ const logeame = () => {
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
                 <InputText 
-                    className={credencialesError.emailError === ""
-                    ? "inputBasicDesign"
-                    : "inputBasicDesign inputErrorDesign"}
+                    
                     type={'text'}
                     name={'email'}
                     placeholder={'Email...'}
@@ -134,9 +176,7 @@ const logeame = () => {
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <InputText 
-                    className={credencialesError.passwordError === ""
-                    ? "inputBasicDesign"
-                    : "inputBasicDesign inputErrorDesign"}
+                    
                     type={'password'}
                     name={'password'}
                     placeholder={'Password...'}
