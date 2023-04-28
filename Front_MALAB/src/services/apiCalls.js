19ddfae0ac7c1dfa79a6835ed90051874e23a7b4
Mario Aguilar <marioaguilar740@gmail.com>
2023-04-28 11:35:53 +0200

    Se ha creado la peticion para crear un nuevo usuario

--- Front_MALAB/src/services/apiCalls.js
+++ Front_MALAB/src/services/apiCalls.js
@@ -5,3 +5,7 @@ const root = "http://localhost:3000"
 export const logMe = async (body) => {
     return await axios.post(`${root}/api/login`, body);
 }
+
+export const nuevoUsuario = async (body) => {
+    return await axios.post(`${root}/api/nuevousuario`, body)
+}
