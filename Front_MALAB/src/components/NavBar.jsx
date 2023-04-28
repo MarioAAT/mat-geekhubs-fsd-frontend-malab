bca07419f80f67bffb31e7db0a46878e0a589c2d
Mario Aguilar <marioaguilar740@gmail.com>
2023-04-28 11:12:05 +0200

    Corrección de id en NavBar

--- Front_MALAB/src/components/NavBar.jsx
+++ Front_MALAB/src/components/NavBar.jsx
@@ -12,10 +12,14 @@ export const NavBar = () => {
     const credentialsRdx = useSelector(userData);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    
     const logout = () => {
+    localStorage.setItem("token", JSON.stringify(null));
         dispatch(userout({ credentials: {}, token: ''}));
         return navigate("/");
     }
+
+    console.log(credentialsRdx)
     return (
         <>
         <Navbar bg="light" expand="lg">
@@ -24,16 +28,46 @@ export const NavBar = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="#home">Coworking</Nav.Link>
-                <Nav.Link href="#link">Mesas</Nav.Link>
-                <Nav.Link href="#link">Tarifas</Nav.Link>
-                <Nav.Link href="#link">Contacto</Nav.Link>
-                <NavDropdown title="Únete a la Familia" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Soy miembro</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Quiero unirme</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Quiero visitar MALAB</NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="#link"></Nav.Link>
+                {credentialsRdx.credentials.id_rol === 2 ? (
+                    <>
+                    <Nav.Link href="#home">Coworking</Nav.Link>
+                    <Nav.Link href="#link">Mesas</Nav.Link>
+                    <Nav.Link href="#link">Tarifas</Nav.Link>
+                    <Nav.Link href="#link">Contacto</Nav.Link>
+                    <Nav.Link href="#link">Reservar</Nav.Link>
+                    <NavDropdown title="Mi espacio" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Mi perfil</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Mis reservas</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to='/logout' onClick={() => logout()}>Logout</NavDropdown.Item>
+                    </NavDropdown>
+                    </>
+                ) : credentialsRdx.credentials.id_rol === 1 ? (
+                    <>
+                    <Nav.Link href="#home">Coworking</Nav.Link>
+                    <Nav.Link href="#link">Mesas</Nav.Link>
+                    <Nav.Link href="#link">Tarifas</Nav.Link>
+                    <Nav.Link href="#link">Cursos</Nav.Link>
+                    <Nav.Link href="#link">Reservar</Nav.Link>
+                    <NavDropdown title="Familia MALAB" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Mi perfil</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Usuarios</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Reservas</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to='/logout' onClick={() => logout()}>Logout</NavDropdown.Item>
+                    </NavDropdown>
+                    </>
+                ) : (
+                    <>
+                    <Nav.Link href="#home">Coworking</Nav.Link>
+                    <Nav.Link href="#link">Mesas</Nav.Link>
+                    <Nav.Link href="#link">Tarifas</Nav.Link>
+                    <Nav.Link href="#link">Contacto</Nav.Link>
+                    <NavDropdown title="Únete a la Familia" id="basic-nav-dropdown">
+                    <NavDropdown.Item as={Link} to='/login'>Soy miembro</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to='/registro'>Quiero unirme</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Quiero visitar MALAB</NavDropdown.Item>
+                    </NavDropdown>
+                    </>
+                )}                
             </Nav>
             </Navbar.Collapse>
         </Container>
