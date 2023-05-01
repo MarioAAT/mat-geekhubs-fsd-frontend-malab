dbfd68aa5bbf205dcddebfc3cf5e0e8445c45b15
Mario Aguilar <marioaguilar740@gmail.com>
2023-05-01 12:57:27 +0200

    Se han hecho ajustes css en el Carousel y agregado imagenes

--- Front_MALAB/src/components/Carousel.jsx
+++ Front_MALAB/src/components/Carousel.jsx
@@ -1,58 +1,35 @@
 import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Image } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 import './Carousel.css'
+import  chicataza  from '../images/chicataza.jpg';
+import estantes from '../images/estantes.jpg';
+import tornocorazon from '../images/tornocorazon.jpg';
 
 export const Caroussel = () => {
     return (
         <>
-        <Container className="carousel-wrapper">
+        <Container className="carousel-wrapper mt-5">
             <Row>
                 <Col className=''>
                 <Carousel interval={5000} >
                     <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://via.placeholder.com/400x200.png?text=First+Slide"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        {/* <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-                    </Carousel.Caption>
+                        <Image src={chicataza} className='img-fluid'/>
                     </Carousel.Item>
                     <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://via.placeholder.com/400x200.png?text=Second+Slide"
-                        alt="Second slide"
-                    />
-
-                    <Carousel.Caption>
-                        {/* <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-                    </Carousel.Caption>
+                        <Image src={estantes} className='img-fluid'/>
                     </Carousel.Item>
                     <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://via.placeholder.com/400x200.png?text=Third+Slide"
-                        alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                        {/* <h3>Third slide label</h3>
-                        <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p> */}
-                    </Carousel.Caption>
+                        <Image src={tornocorazon} className='img-fluid'/>
                     </Carousel.Item>
                 </Carousel>
                 </Col>
                 <Col lg={6} className="d-flex flex-column justify-content-center align-items-start">
-                <h2>Servicios</h2>
-                <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam erat volutpat. Nunc dictum nibh a felis maximus pretium. Integer sagittis, tortor ac gravida tristique, odio justo tincidunt arcu, vel ultrices velit velit vitae ipsum. Nulla facilisi. Sed at risus felis.</p>
-            </Col>
+                <h2>MALAB</h2>
+                <p className=''>Con el fin de crear comunidad y expandir los conocimientos de la cerámica nace la propuesta 
+                                MALAB CERAMIC SURFACE LABORATORY BY PRODESCO. Un espacio donde dar rienda suelta a la 
+                                creatividad a través de la experimentación con materiales cerámicos.</p>
+                </Col>
             </Row>
         </Container>
         </>
