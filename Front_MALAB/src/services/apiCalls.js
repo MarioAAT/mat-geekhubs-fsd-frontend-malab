fd6770889e115d6222bbc78881ab82417ad93d1b
Mario Aguilar <marioaguilar740@gmail.com>
2023-05-02 19:24:55 +0200

    Se hizo la función post para que se guardara en la BBDD la reserva

--- Front_MALAB/src/services/apiCalls.js
+++ Front_MALAB/src/services/apiCalls.js
@@ -9,3 +9,11 @@ export const logMe = async (body) => {
 export const nuevoUsuario = async (body) => {
     return await axios.post(`${root}/api/nuevousuario`, body)
 }
+
+export const nuevaReserva = async ( body, token) => {
+    let config = {
+        headers: { 
+            'Authorization': 'Bearer '+ token,  
+        }};
+        return await axios.post(`${root}/api/nuevareserva`,body, config)
+}
