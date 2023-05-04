77e1dea9f2d692606952c3931280c04944570ec1
Mario Aguilar <marioaguilar740@gmail.com>
2023-05-04 16:41:12 +0200

    Se han agregado las nuevas rutas para ver todos los usuarios y todas las reservas

--- Front_MALAB/src/layouts/mainApp/MainApp.jsx
+++ Front_MALAB/src/layouts/mainApp/MainApp.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { Footer } from '../../components/Footer';
 import { NavBar } from '../../components/NavBar';
+import { AllUsuarios } from '../allUsuarios/AllUsuarios';
+import { UsuarioDetail } from '../allUsuarios/UsuarioDetail';
 import { Home } from '../home/Home';
 import { Login } from '../login/Login';
 import { Register } from '../register/Register';
+import { AllReservas } from '../reservas/AllReservas';
 import { Reservas } from '../reservas/Reservas';
 
 export const MainApp = () => {
@@ -16,6 +19,9 @@ export const MainApp = () => {
         <Route path='/login' element={<Login/>}/>
         <Route path='/registro' element={<Register/>}/>
         <Route path='/reserva' element={<Reservas/>}/>
+        <Route path='/allusuarios' element={<AllUsuarios/>}/>
+        <Route path='/infousuario' element={<UsuarioDetail/>}/>
+        <Route path='/allreservas' element={<AllReservas/>}/>
         <Route path='*' element={<Navigate to="/"/>}/>
     </Routes>
     <Footer/>
