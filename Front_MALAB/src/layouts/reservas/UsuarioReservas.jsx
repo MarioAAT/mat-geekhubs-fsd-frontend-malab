dbe74f471a0ffe305a145e29a8b53ae8fe18b74d
Mario Aguilar <marioaguilar740@gmail.com>
2023-05-08 15:01:02 +0200

    Se han arreglado las reservas de usuarios, las renderiza adecudamente

---
+++ Front_MALAB/src/layouts/reservas/UsuarioReservas.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import  CardReserva  from '../../components/CardReserva';
+import { getReservaUsuario } from '../../services/apiCalls';
+import { userData } from '../userDetail/userSlice';
+
+export const UsuarioReservas = () => {
+
+    const ReduxCredentials = useSelector(userData);
+    const dispatch = useDispatch();
+
+    const [reserva, setReserva] = useState([]);
+    
+    useEffect(() => {
+        if(reserva.length === 0) {
+            getReservaUsuario(ReduxCredentials.credentials.token)
+            .then((result) => {
+                setReserva(result.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        }
+    },[reserva]);
+    console.log(reserva)
+
+    return (
+        <>
+            <Container>
+            {reserva.length > 0 ? 
+                (<Row>
+                    {reserva.map( (reservas) => {
+                        return (
+                            <Col key={reservas.id}>
+                                <CardReserva appo={reservas}/>
+                            </Col>
+                        );
+                    })}
+                </Row>)
+                : 
+                (<div>......</div>)
+            }        
+            </Container>
+        </>
+    )
+}
