58ea45bf1db768913f07e51e17b02bf57c057953
Mario Aguilar <marioaguilar740@gmail.com>
2023-05-08 16:23:52 +0200

    Se ha agregado la ruta de editar perfil de usuario como Admin

--- Front_MALAB/src/layouts/mainApp/MainApp.jsx
+++ Front_MALAB/src/layouts/mainApp/MainApp.jsx
@@ -6,9 +6,13 @@ import { AllUsuarios } from '../allUsuarios/AllUsuarios';
 import { UsuarioDetail } from '../allUsuarios/UsuarioDetail';
 import { Home } from '../home/Home';
 import { Login } from '../login/Login';
+import { AdminEditPerfil } from '../perfilUsuario/AdminEditPerfil';
+import { EditarPerfil } from '../perfilUsuario/EditarPerfil';
+import { PerfilUsuario } from '../perfilUsuario/PerfilUsuario';
 import { Register } from '../register/Register';
 import { AllReservas } from '../reservas/AllReservas';
 import { Reservas } from '../reservas/Reservas';
+import { UsuarioReservas } from '../reservas/UsuarioReservas';
 
 export const MainApp = () => {
   return (
@@ -22,6 +26,10 @@ export const MainApp = () => {
         <Route path='/allusuarios' element={<AllUsuarios/>}/>
         <Route path='/infousuario' element={<UsuarioDetail/>}/>
         <Route path='/allreservas' element={<AllReservas/>}/>
+        <Route path='/perfil' element={<PerfilUsuario/>}/>
+        <Route path='/misreservas' element={<UsuarioReservas/>}/>
+        <Route path='/editperfil' element={<EditarPerfil/>}/>
+        <Route path='/admineditperfil' element={<AdminEditPerfil/>}/>
         <Route path='*' element={<Navigate to="/"/>}/>
     </Routes>
     <Footer/>
